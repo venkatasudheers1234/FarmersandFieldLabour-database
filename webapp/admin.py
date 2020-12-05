eef53a558a53d0ba034b9b41644167b117891c7f
venkatasudheers1234 <46993528+venkatasudheers1234@users.noreply.github.com>
2020-12-05 16:44:15 +0530

    admin.py

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,1 +1,10 @@
+from django.contrib import admin
+from . models import fieldlabour
+from . models import farmer
+
+
+# Register your models here.
+admin.site.register(fieldlabour)
+admin.site.register(farmer)
+
 
